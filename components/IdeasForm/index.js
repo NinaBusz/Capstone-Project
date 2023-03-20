63f526b0736670d59d727af16d0e87eaacd4a0fc
Nina Busz <buss.nina@googlemail.com>
2023-03-20 12:38:49 +0100

    initial commit

---
+++ components/IdeasForm/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Ideasform({}) {
+  return (
+    <>
+      {" "}
+      <styleslabel htmlFor="idea">Fixe Idee:</styleslabel>{" "}
+      <input name="idea" type="text" id="idea" />{" "}
+    </>
+  );
+}
