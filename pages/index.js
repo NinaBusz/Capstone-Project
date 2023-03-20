0099de93e550e4a298947ead500f21b9e6dae35b
Nina Busz <buss.nina@googlemail.com>
2023-03-20 12:45:27 +0100

    add component Ideasform to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Heading from "../components/Heading";
+import Ideasform from "../components/Ideasform";
 
 export default function Home() {
   return (
     <main>
       <Heading>🔱Atlantis Capstone Template🔱</Heading>
+      <Ideasform />
     </main>
   );
 }
