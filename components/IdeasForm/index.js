62ff3ba428fddd517d56568af12e8fa8e1e8acae
Nina Busz <buss.nina@googlemail.com>
2023-03-20 15:33:50 +0100

    add initial styling

--- components/IdeasForm/index.js
+++ components/IdeasForm/index.js
@@ -1,12 +1,12 @@
 import React from "react";
+
 export default function Ideasform({}) {
   return (
     <>
-      {" "}
-      <styleslabel htmlFor="idea">Fixe Idee:</styleslabel>{" "}
-      <input name="idea" type="text" id="idea" placeholder="Hier tippen..." />{" "}
-      <button type="save">speichern</button>{" "}
-      <button type="save">verwerfen</button>
+      <label htmlFor="idea">Fixe Idee:</label>
+      <input name="idea" type="text" id="idea" placeholder="Hier tippen..." />
+      <button type="save">speichern</button>
+      <button type="delete">verwerfen</button>
     </>
   );
 }
--- styles.js
+++ styles.js
@@ -5,9 +5,26 @@ export default createGlobalStyle`
   *::before,
   *::after {
     box-sizing: border-box;
+    background-color: #11121B;
+    color: #C3DAE8;
+  }
+  h1{
+    color:#FF2B44;
   }
   body {
-    margin: 0;
+    margin-right: 10px;
+    margin-left: 10px;
     font-family: system-ui;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
   }
+  input {
+    width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  }
+  button {padding: 5px 10px;}
 `;
